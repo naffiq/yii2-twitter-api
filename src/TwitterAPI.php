1482eb3bada7832c720f5e6ef6e283a5a6f19e9d
naffiq <abdu.galymzhan@gmail.com>
2017-03-30 18:31:13 +0600

    Init code commit

---
+++ src/TwitterAPI.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: naffiq
+ * Date: 3/30/17
+ * Time: 6:11 PM
+ */
+
+namespace naffiq\twitterapi;
+
+use yii\base\Component;
+
+/**
+ * Class TwitterAPI
+ *
+ * Initializes [[\TwitterAPIExchange]] object and provides layer to it's methods
+ *
+ * @package naffiq\twitterapi
+ */
+class TwitterAPI extends Component
+{
+    public $oauthAccessToken;
+    public $oauthAccessTokenSecret;
+
+    public $consumerKey;
+    public $consumerSecret;
+
+    /**
+     * @var \TwitterAPIExchange
+     */
+    private $twitterApiExchange;
+
+    /**
+     * @inheritdoc
+     *
+     * Initializes TwitterAPIExchange object for further usage
+     */
+    public function init()
+    {
+        parent::init();
+
+        $settings = [
+            'oauth_access_token' => $this->oauthAccessToken,
+            'oauth_access_token_secret' => $this->oauthAccessTokenSecret,
+            'consumer_key' => $this->consumerKey,
+            'consumer_secret' => $this->consumerSecret
+        ];
+
+        $this->twitterApiExchange = new \TwitterAPIExchange($settings);
+    }
+
+    /**
+     * Returns TwitterAPIExchange object
+     *
+     * @return \TwitterAPIExchange
+     */
+    public function exchange()
+    {
+        return $this->twitterApiExchange;
+    }
+
+    /**
+     * Direct access to [[\TwitterAPIExchange::setPostfields()]] method
+     * @see \TwitterAPIExchange::setPostfields()
+     *
+     * @param array $array
+     * @return \TwitterAPIExchange
+     */
+    public function setPostfields(array $array)
+    {
+        return $this->twitterApiExchange->setPostfields($array);
+    }
+
+    /**
+     * Direct access to [[\TwitterAPIExchange::setGetfield()]] method
+     * @see \TwitterAPIExchange::setGetfield()
+     *
+     * @param string $string
+     * @return \TwitterAPIExchange
+     */
+    public function setGetfield($string)
+    {
+        return $this->twitterApiExchange->setGetfield($string);
+    }
+
+    /**
+     * Direct access to [[\TwitterAPIExchange::getGetField()]] method
+     * @see \TwitterAPIExchange::getGetfield()
+     *
+     * @return string
+     */
+    public function getGetfield()
+    {
+        return $this->twitterApiExchange->getGetfield();
+    }
+
+    /**
+     * Direct access to [[\TwitterAPIExchange::getPostfields()]] method
+     * @see \TwitterAPIExchange::getPostfields()
+     *
+     * @return array
+     */
+    public function getPostfields()
+    {
+        return $this->twitterApiExchange->getPostfields();
+    }
+
+    /**
+     * Direct access to [[\TwitterAPIExchange::buildOauth()]] method
+     * @see \TwitterAPIExchange::buildOauth()
+     *
+     * @param string $url
+     * @param string $requestMethod
+     * @return \TwitterAPIExchange
+     */
+    public function buildOauth($url, $requestMethod)
+    {
+        return $this->twitterApiExchange->buildOauth($url, $requestMethod);
+    }
+
+    /**
+     * Direct access to [[\TwitterAPIExchange::performRequest()]] method
+     * @see \TwitterAPIExchange::performRequest()
+     *
+     * @param bool $return
+     * @param array $curlOptions
+     * @return string
+     */
+    public function performRequest($return = true, $curlOptions = [])
+    {
+        return $this->twitterApiExchange->performRequest($return, $curlOptions);
+    }
+
+    /**
+     * Direct access to [[\TwitterAPIExchange::request()]] method
+     * @see \TwitterAPIExchange::request()
+     *
+     * @param string $url
+     * @param string $method
+     * @param null $data
+     * @param array $curlOptions
+     * @return string
+     */
+    public function request($url, $method = 'get', $data = null, $curlOptions = [])
+    {
+        return $this->twitterApiExchange->request($url, $method, $data, $curlOptions);
+    }
+}
