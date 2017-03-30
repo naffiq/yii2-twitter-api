3af3473412d0d526782d1d22d18c2d9894d0263f
naffiq <abdu.galymzhan@gmail.com>
2017-03-30 21:49:58 +0600

    added phpunit backward compitability

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,11 @@ define('YII_DEBUG', true);
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require_once(__DIR__ . '/../src/TwitterAPI.php');
